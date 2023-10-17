4c24afd40ec033dff17de042c24075688c247c0d
Iryna Borniak <la.aerea.1@gmail.com>
2023-10-17 13:11:22 +0300

    change single quotes to double ones

--- spec/views/books/edit.html.tailwindcss_spec.rb
+++ spec/views/books/edit.html.tailwindcss_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'books/edit', type: :view do
+RSpec.describe "books/edit", type: :view do
   let(:book) do
     Book.create!(
-      title: 'MyString',
-      author: 'MyString',
-      isbn: '1234567890123',
-      description: 'MyText'
+      title: "MyString",
+      author: "MyString",
+      isbn: "1234567890123",
+      description: "MyText"
     )
   end
 
@@ -14,17 +14,17 @@ RSpec.describe 'books/edit', type: :view do
     assign(:book, book)
   end
 
-  it 'renders the edit book form' do
+  it "renders the edit book form" do
     render
 
-    assert_select 'form[action=?][method=?]', book_path(book), 'post' do
-      assert_select 'input[name=?]', 'book[title]'
+    assert_select "form[action=?][method=?]", book_path(book), "post" do
+      assert_select "input[name=?]", "book[title]"
 
-      assert_select 'input[name=?]', 'book[author]'
+      assert_select "input[name=?]", "book[author]"
 
-      assert_select 'input[name=?]', 'book[isbn]'
+      assert_select "input[name=?]", "book[isbn]"
 
-      assert_select 'textarea[name=?]', 'book[description]'
+      assert_select "textarea[name=?]", "book[description]"
     end
   end
 end
