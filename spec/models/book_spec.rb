45324034b9d07f70e5bff4ce35b74ed6b59e5566
Iryna Borniak <la.aerea.1@gmail.com>
2023-10-19 03:22:57 +0300

    refactor model test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:author) }
-  it { should validate_presence_of(:isbn) }
-  it { should validate_uniqueness_of(:isbn) }
-  it { should validate_presence_of(:description) }
-  it { should validate_numericality_of(:isbn).only_integer }
-  it { should validate_length_of(:isbn).is_equal_to(13) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:author) }
+  it { is_expected.to validate_presence_of(:isbn) }
+  it { is_expected.to validate_uniqueness_of(:isbn) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_numericality_of(:isbn).only_integer }
+  it { is_expected.to validate_length_of(:isbn).is_equal_to(13) }
 end
