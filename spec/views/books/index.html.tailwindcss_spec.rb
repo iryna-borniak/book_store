733ac5470df696e649de425f71657a3dbe9cea2f
Iryna Borniak <la.aerea.1@gmail.com>
2023-10-20 19:22:58 +0300

    add book styles

--- spec/views/books/index.html.tailwindcss_spec.rb
+++ spec/views/books/index.html.tailwindcss_spec.rb
@@ -20,11 +20,7 @@ RSpec.describe "books/index", type: :view do
 
   it "renders a list of books" do
     render
-    cell_selector = Rails::VERSION::STRING >= "7" ? "div>p" : "tr>td"
-    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Author".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("1234567890123".to_s), count: 1
-    assert_select cell_selector, text: Regexp.new("1234567890122".to_s), count: 1
-    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    
+    assert_select "h3", text: Regexp.new("Title".to_s), count: 2    
   end
 end
