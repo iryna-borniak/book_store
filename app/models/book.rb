d4e465bc67406b5b260174663c8d08a7ccf4af96
Iryna Borniak <la.aerea.1@gmail.com>
2023-10-29 02:04:42 +0300

    refactor: Book model ordering

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   validates :title, :author, :description, presence: true
   validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }, length: { is: 13 }
 
-  scope :ordered_by_title, -> { order(title: :asc) }
+  scope :ordered_by_title, -> { order(:title) }
 end
