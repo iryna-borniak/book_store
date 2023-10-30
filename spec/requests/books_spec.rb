5cffbe621930e01ea8dc840aaa8f103a46fcb537
Iryna Borniak <la.aerea.1@gmail.com>
2023-10-30 15:12:16 +0200

    refactor tests

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe BooksController, type: :request do
   let!(:book) { FactoryBot.create(:book) }
 
   describe "GET #index" do
-    it "renders a successful response" do      
+    it "renders a successful response" do
       get books_url
 
       expect(response).to be_successful
@@ -50,12 +50,11 @@ RSpec.describe BooksController, type: :request do
   describe "POST #create" do
     context "with valid parameters" do
       it "creates a new Book and redirects to the created book" do
-
         expect do
           post books_url, params: { book: valid_attributes }
         end.to change(Book, :count).by(1)
 
-        expect(response).to redirect_to(book_url(Book.last))
+        expect(response).to be_redirect
 
         expect(flash[:notice]).to eq("Book was successfully created.")
       end
@@ -63,7 +62,6 @@ RSpec.describe BooksController, type: :request do
 
     context "with invalid parameters" do
       it "does not create a new Book and renders a response with 422 status ('new' template)" do
-
         expect do
           post books_url, params: { book: invalid_attributes }
         end.not_to change(Book, :count)
@@ -76,13 +74,12 @@ RSpec.describe BooksController, type: :request do
   describe "PATCH #update" do
     context "with valid parameters" do
       it "updates the requested book and redirects" do
-        
         expect do
           patch book_url(book), params: { book: new_attributes }
 
           book.reload
         end.to change(book, :title).to(new_attributes[:title])
-               
+
         expect(response).to redirect_to(book_url(book))
 
         expect(flash[:notice]).to eq("Book was successfully updated.")
@@ -100,13 +97,12 @@ RSpec.describe BooksController, type: :request do
 
   describe "DELETE #destroy" do
     it "destroys the requested book and redirects" do
-      
       expect do
         delete book_url(book)
       end.to change(Book, :count).by(-1)
 
       expect(response).to redirect_to(books_url)
-      
+
       expect(flash[:alert]).to eq("Book was successfully destroyed.")
     end    
   end
