90c0523b4183eb514bb488927bcecb6beb609a05
Iryna Borniak <la.aerea.1@gmail.com>
2023-11-09 02:38:55 +0200

    add devise views, controller

--- db/migrate/20231108000642_devise_create_users.rb
+++ db/migrate/20231108000642_devise_create_users.rb
@@ -6,13 +6,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable      
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: "" 
-      t.string :username,           null: false, default: ""
-      t.string :first_name,         null: false, default: ""
-      t.string :last_name,          null: false, default: ""     
+      t.string :username,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false     
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -41,7 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :username,             unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,16 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_000642) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "username", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
