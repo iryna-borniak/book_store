e6bc7c091060059f76a8d7939a68de42f7c5e758
Iryna Borniak <la.aerea.1@gmail.com>
2023-11-19 04:01:50 +0200

    add logic for image and files uploads to Book model and controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_one_attached :cover_image
+  has_one_attached :pdf_file
+
   validates :title, :author, :description, presence: true
   validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }, length: { is: 13 }
 
