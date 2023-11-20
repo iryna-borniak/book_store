cd15c0d3b5dbb9e5c9f55d531ca9ed8dbea2c121
Iryna Borniak <la.aerea.1@gmail.com>
2023-11-20 21:38:55 +0200

    add tests for attached files

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   has_one_attached :pdf_file
 
   validates :title, :author, :description, presence: true
-  validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }, length: { is: 13 }
+  validates :isbn, presence: true, uniqueness: { case_sensitive: false }, numericality: { only_integer: true }, length: { is: 13 }
 
   scope :ordered_by_title, -> { order(:title) }
 end
