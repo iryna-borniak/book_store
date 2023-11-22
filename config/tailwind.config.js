23c9f00db931d97103fe623ca4e319bb3d398ce6
Iryna Borniak <la.aerea.1@gmail.com>
2023-11-22 15:27:47 +0200

    refactor styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,11 +18,23 @@ module.exports = {
         'banner-gradient': 'linear-gradient(90deg, #0045ff, #16dfda)',
         'show-gradient': 'linear-gradient(180deg, #f7f7f7, #ffffff)',
         'main-banner-gradient': 'linear-gradient(135deg, #0071eb 30%, #14d4d8)',
+        'ring': "url('ring.svg')",
       },      
       width: {
         'show-title-width': 'calc(66.666667% - 40px)',
-        'book-info-width': 'calc(100% - 159px)',                
+        'book-info-width': 'calc(100% - 159px)',
+        '580': '580px',                
       },
+      maxWidth: {
+        'content-md': '980px',
+      },
+      height: {
+        '600': '600px',
+      },
+      inset: {
+        '50': '50px',
+        '120': '120px',
+      },           
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
